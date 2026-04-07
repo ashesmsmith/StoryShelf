d1e25d4d3214cac1e035d4b5909ce2a2bf793371
riro9510 <riro9510@gmail.com>
2026-04-07 16:07:52 -0600

    updating prisma route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '@/app/lib/prisma';
+import prisma from '@/lib/prisma';
 import { comparePassword } from '@/lib/auth';
 
 export async function POST(req: Request) {
